a776abe9ca54db5012587d300f614c7cf966ce52
kocho31 <99873300+kocho31@users.noreply.github.com>
2025-04-19 19:03:48 +0100

    main

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,18 @@
+import ButtonAnimated from "./components/ButtonAnimated";
+import ButtonFadeUp from "./components/ButtonFadeUp";
+import ButtonFadeUpOnce from "./components/ButtonFadeUpOnce";
+import ButtonFadeUpSimple from "./components/ButtonFadeUpSimple";
+import MarqueeButton from "./components/MarqueeButton";
 
 
 export default function Home() {
   return (
-    <div>
-
+    <div className="flex flex-col items-center justify-center h-screen">
+    <ButtonAnimated pageHref="/sobre" />
+    <MarqueeButton />
+    <ButtonFadeUp />
+    <ButtonFadeUpOnce />
+    <ButtonFadeUpSimple />
     </div>
   );
 }
---
+++ src/app/sobre/page.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function SobrePage() {
+  return (
+    <div>SobrePage</div>
+  )
+}
