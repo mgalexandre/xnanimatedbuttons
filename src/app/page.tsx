6a9d83ef4c1a41aa9b5b62c0766d53a666ec83f7
Alexandre Martins <alexandre_martins_pbl@hotmail.com>
2025-05-13 10:48:04 +0100

    iconmoon

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,12 +8,33 @@ import MarqueeButton from "./components/MarqueeButton";
 
 
 export default function Home() {
+
+
+
   return (
+    <>
+    <div>
+      <div className="bg-pink-400 h-10 flex justify-around items-center">
+      <span className="icon icon-images"></span>
+      <span className="icon icon-camera"></span>
+      <span className="icon icon-headphones"></span>
+      <span className="icon icon-music"></span>
+      <span className="icon icon-play"></span>
+      <span className="icon icon-film"></span>
+      <span className="icon icon-video-camera"></span>
+      <span className="icon icon-dice"></span>
+      <span className="icon icon-pacman"></span>
+      <span className="icon icon-spades"></span>
+      </div>
+    </div>
+
+
     <div className="flex flex-col gap-4 items-center justify-center h-screen">
       <MarqueeButton />
       <ButtonFadeUp />
       <ButtonFadeUpOnce />
       <ButtonFadeUpSimple />
     </div>
+    </>
   );
 }
